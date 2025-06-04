d29ab0568e30909fecf6d6c45cd5ba025a72b311
Oğuzhan YALÇIN <oguzhan@post.com>
2025-06-04 13:56:23 +0300

    first init

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,72 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div>
+      <div className="flex flex-col items-center justify-center">
+        <Image src={"/logo.svg"} alt="logo" width={230} height={153} className="mt-1.5 m-auto" />
+      </div>
+      <div className="flex flex-row items-center justify-around mt-4">
+        <Card className="w-4xl">
+          <CardHeader>
+            <CardTitle>Hesabınıza giriş yapın</CardTitle>
+            <CardDescription>
+              <div className="flex justify-between items-center">
+                Bütçe yönetimi için giriş yapın veya yeni bir hesap oluşturun.
+                <Button variant="link">Kayıt Ol</Button>
+              </div>
+            </CardDescription>
+            <CardAction>
+            </CardAction>
+          </CardHeader>
+          <CardContent>
+            <div className="flex justify-between items-center">
+              <Image src={"/banner.jpg"} alt="banner" width={500} height={153} className=" rounded-xl" />
+              <div className="flex items-center m-auto" >
+                <form>
+                  <div className="flex flex-col gap-6">
+                    <div className="grid gap-2">
+                      <Label htmlFor="email">E-Posta</Label>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="elon@tesla.com"
+                        required
+                      />
+                    </div>
+                    <div className="grid gap-2">
+                      <div className="flex items-center">
+                        <Label htmlFor="password">Parola</Label>
+                      </div>
+                      <Input id="password" type="password" placeholder="tesla123" required />
+                      <Button type="submit" className="w-full">
+                        Giriş Yap
+                      </Button>
+                      <a
+                        href="#"
+                        className="text-center text-sm underline-offset-2 hover:underline"
+                      >
+                        Şifreni mi unuttun?
+                      </a>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter className="flex-col gap-3 mt-2">
+            <p className="text-sm text-muted-foreground">
+              made with 🩷 by <a className="font-semibold hover:underline" href="https://github.com/oguzhan-developer" >Oğuzhan YALÇIN</a>
+            </p>
+          </CardFooter>
+        </Card>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+      </div>
     </div>
   );
 }
